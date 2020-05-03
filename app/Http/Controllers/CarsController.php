6945cac75af88f07e61567419cca151dd110c4cd
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-05-03 17:17:18 -0500

    cars updated

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -17,9 +17,20 @@ class CarsController extends Controller
         // $hash = $request->header('Authorization', null);
     }
 
-    public function index(Request $request) {
-        echo "index";
-        die();
+    public function index() {
+        $cars = Car::all()->load('user');
+        return response()->json(array(
+            'cars' => $cars,
+            'status' => 'success',
+        ), 200);
+    }
+
+    public function show($car_id) {
+        $car = Car::find($car_id)->load('user');
+        return response()->json(array(
+            'car' => $car,
+            'status' => 'success',
+        ), 200);
     }
 
     public function store(Request $request) {
@@ -72,5 +83,50 @@ class CarsController extends Controller
 
         return response()->json($data);
 
+    }
+
+    public function update($car_id, Request $request) {
+        // $jwtAuth = new JwtAuth();
+        // $hash = $request->header('Authorization', null);
+        
+        $json = $request->input('json', null);
+        // $params = json_decode($json);
+        $params_array = json_decode($json,true);
+        
+        // $user = $jwtAuth->checkToken($hash, true);
+        
+        $request->merge($params_array);
+        
+        try {
+            $validate = $this->validate($request, [
+                'title' => 'required|min:5',
+                'description' => 'required',
+                'price' => 'required',
+                'status' => 'required',
+            ]);
+
+        } catch (\Illuminate\Validation\ValidationException $e) {
+
+            $errors = '';
+            foreach($e->errors() as $key => $value) {
+                $errors .= $value[0];
+            }
+            $data = array(
+                'error' => $errors,
+                'status' => 'error',
+                'code' => 400,
+            );
+            return $data;
+        }
+
+        $car = Car::where('id',$car_id)->update($params_array);
+        $car = Car::find($car_id);
+        $data = array(
+            'car' => $car,
+            'status' => 'success',
+            'code' => 200,
+        );
+
+        return response()->json($data);   
     }
 }
